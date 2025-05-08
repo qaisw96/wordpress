8b4fae11206e0b21d09433d87490eeda9eb39a39
qaisw96 <qaisw14@gmail.com>
2025-05-09 02:03:57 +0300

    databse congig

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,32 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** Database hostname */
+define( 'DB_NAME', 'u191253903_root' );
+define( 'DB_USER', 'u191253903_root' );
+define( 'DB_PASSWORD', 'ASDasd#131517' );
 define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+// $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+
+// if (in_array($host, ['localhost', '127.0.0.1'])) {
+//     // Local DB settings
+//     define('DB_NAME', 'wordpress');
+//     define('DB_USER', 'root');
+//     define('DB_PASSWORD', 'root');
+//     define('DB_HOST', 'localhost');
+// } else {
+//     // Production (Hostinger) DB settings
+//     define('DB_NAME', 'your_hostinger_db_name');
+//     define('DB_USER', 'your_hostinger_db_user');
+//     define('DB_PASSWORD', 'your_hostinger_db_password');
+//     define('DB_HOST', 'localhost'); // Usually still localhost on shared hosting
+// }
+
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
+
 
 /**#@+
  * Authentication unique keys and salts.
